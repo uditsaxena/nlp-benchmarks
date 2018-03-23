939c219c69524a08b7dace2ef38fac7dc0782aa6
Udit Saxena <saxena.udit@gmail.com>
2018-03-23 14:54:16 -0400

    Optimized nn construction

--- src/construct_graph_mm.py
+++ src/construct_graph_mm.py
@@ -1,30 +1,65 @@
-import scipy
-import torch
 import numpy as np
+import torch
 
-from src.graphconv import graph, coarsening
+
+def dist_calculation(tensor):
+    # print(torch.mm(tensor, tensor.t()))
+    size = tensor.shape
+    print("Size : ", size)
+    batch_size = int(size[0] / 64)
+    rows = size[0]
+    i = 0
+    num_batches = int(rows / batch_size) + 1
+    if rows % batch_size == 0:
+        num_batches -= 1
+    # print(tensor)
+    num_edges = 3
+    nn_list = []
+    for _ in range(num_batches):
+        print(i)
+        begin = i
+        end = begin + batch_size
+        if end > rows:
+            end = rows
+        x_tensor = tensor[begin:end, :]
+        curr_batch_mul_list = []
+        tensor_batch_dist_list = []
+        for k in range(batch_size):
+            curr_tensor = x_tensor[k, :]
+            j = 0
+            curr_tensor_dist_list = []
+            for _ in range(num_batches):
+                # print(i, j)
+                mul_begin = j
+                mul_end = mul_begin + batch_size
+                if mul_end > rows:
+                    mul_end = rows
+                y_tensor = tensor[mul_begin: mul_end, :]
+                # print(y_tensor)
+                dist = y_tensor - curr_tensor
+                curr_tensor_batch_dist = torch.sum(torch.pow(dist, 2), dim=1)
+                # print(curr_tensor_batch_dist)
+                curr_tensor_dist_list.append(curr_tensor_batch_dist)
+                j = mul_end
+            curr_tensor_batch_dist_tensor = torch.cat(curr_tensor_dist_list, 0)
+            # print(curr_tensor_batch_dist_tensor)
+            tensor_batch_dist_list.append(curr_tensor_batch_dist_tensor.view(1, -1))
+        # print("--")
+        batch_mul_tensor = torch.cat(tensor_batch_dist_list, 0)
+        # print(batch_mul_tensor)
+        values, indices = torch.sort(batch_mul_tensor, 1, descending=True)
+        nn_list.append(indices[:, 0:num_edges + 1])
+        i = end
+    dist_matrix = torch.cat(nn_list)
+    print(dist_matrix.size())
+
 
 if __name__ == '__main__':
     graph_data = np.load("pygcn/ng20_embeddings.npy")
     print("Loaded embeddings... continuing on")
-
     if torch.cuda.is_available():
         mat = torch.FloatTensor(graph_data).cuda()
-        r = torch.mm(mat, mat.t())
-
-    # # print(type(graph_data))
-    # # t_start = time.process_time()
-    # # print("Embedded, starting graph construction")
-    # # number_edges = 16
-    # coarsening_levels = 0
-    # # dist, idx = graph.distance_sklearn_metrics(graph_data, k=number_edges, metric='cosine')
-    # # print('Execution time: {:.2f}s'.format(time.process_time() - t_start))
-    # # A = graph.adjacency(dist, idx)
-    # A = None
-    # # print("{} > {} edges".format(A.nnz // 2, number_edges * graph_data.shape[0] // 2))
-    # A_random = graph.replace_random_edges(A, 0)
-    # graphs, perm = coarsening.coarsen(A_random, levels=coarsening_levels, self_connections=False)
-    # L = [graph.laplacian(A, normalized=True) for A in graphs]
-    # # print(L[0].shape)
-    # # L[0] is a sparse csr Matrix
-    # scipy.sparse.save_npz("pygcn/graph_ng20.npz", L[0])
+        dist_calculation(mat)
+    else:
+        mat = torch.FloatTensor(graph_data)
+        dist_calculation(mat)
